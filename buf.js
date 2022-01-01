d63413313859c9955bfefd2e9bb06f4c61b655e4
Zack Stevens <zack.st7@gmail.com>
2022-01-01 14:26:42 -0700

    fix: Initial release

---
+++ buf.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const {exec} = require("child_process");
+const core = require('@actions/core');
+const minimist = require('minimist')
+
+const args = process.argv.slice(2)
+const parsedArgs = minimist(args)
+const workingDirectory = parsedArgs.workingDirectory
+const bufCommand = parsedArgs.bufCommand
+
+fs.readdir('.', (err, files) => {
+    files.forEach(file => {
+        fs.stat(file, (err, stats) => {
+            if (stats.isDirectory() && file.endsWith('apis')) {
+                if (bufCommand.includes('repository create')) {
+                    const cmd = `buf ${bufCommand}/${file} || true`
+                } else {
+                    const cmd = `cd ${workingDirectory}/${file} && buf ${bufCommand}`
+                }
+                runCommand(cmd)
+            }
+        });
+    });
+});
+
+function runCommand(cmd) {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                core.setFailed(error)
+                reject(error)
+            } else {
+                const result = stdout ? stdout : stderr
+                core.info(result)
+                resolve(result)
+            }
+        });
+    })
+}
